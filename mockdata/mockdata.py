3b999f8102da632c933562ecd62f9c6f9536fa98
Simone Fiorentini <37548538+bluefloyd00@users.noreply.github.com>
2019-11-27 16:23:33 +0000

    PR: fix parameter (#7)
    * added default for schema parameter
    * added test case

--- mockdata/mockdata.py
+++ mockdata/mockdata.py
@@ -5,7 +5,21 @@ import csv
 import mockdata.functions as fx
 
 class csv_mock():
-    def __init__(self, csv, schema):
+    def __init__(self, csv, schema=""):
+        self.file_name = csv
+        self.schema_file = schema
+    
+    def to_json(self):
+        record_list = []
+        if self.schema_file != "":
+            record_list = fx.extract_repeated_records(self.schema_file)
+        df = pd.read_csv(self.file_name)
+        df = df.where(pd.notnull(df), None)
+        return fx.extract_list_dictionary(df, record_list)
+
+
+class sheet_tag_mock():
+    def __init__(self, googlesheet_id, sheet_name, schema):
         self.file_name = csv
         self.schema_file = schema
     
@@ -14,4 +28,3 @@ class csv_mock():
         df = pd.read_csv(self.file_name)
         df = df.where(pd.notnull(df), None)
         return fx.extract_list_dictionary(df, record_list)
-
